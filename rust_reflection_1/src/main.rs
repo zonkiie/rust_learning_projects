4d6d4275bce9bb5fee62754b98e61bd326bd572e
Rainer Datzer <rdatz@gmx.de>
2019-06-10 01:21:03 +0200

    new try

--- rust_reflection_1/src/main.rs
+++ rust_reflection_1/src/main.rs
@@ -1,5 +1,32 @@
-/*fn main() {
-    println!("Hello, world!");
-}
-*/
+/// https://stackoverflow.com/questions/29986057/is-there-is-a-way-to-get-the-field-names-of-a-struct-in-a-macro/29986760#29986760
 
+macro_rules! my_macro {
+    (struct $name:ident {
+        $($field_name:ident: $field_type:ty,)*
+    }) => {
+        struct $name {
+            $($field_name: $field_type,)*
+        }
+
+        impl $name {
+            // This is purely an example—not a good one.
+            fn get_field_names() -> Vec<&'static str> {
+                vec![$(stringify!($field_name)),*]
+            }
+        }
+    }
+}
+
+my_macro! {
+    struct S {
+        a: String,
+        b: String,
+        c: String,
+        d: i64,
+    }
+}
+
+fn main() {
+    //S::get_field_names() == vec!["a", "b"]
+    println!("{:?}", S::get_field_names());
+}
