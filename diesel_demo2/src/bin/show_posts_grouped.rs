117abdaf9124210d09da5d044ed69dbf09becc4b
Rainer Datzer <rdatz@gmx.de>
2021-03-01 03:21:18 +0100

    working on associations

--- diesel_demo2/src/bin/show_posts_grouped.rs
+++ diesel_demo2/src/bin/show_posts_grouped.rs
@@ -9,6 +9,7 @@ extern crate quick_xml;
 use std::*;
 use std::fmt::*;
 use std::error::Error;
+use itertools::*;
 use serde::*;
 use serde::de::*;
 use serde_value::*;
@@ -16,21 +17,24 @@ use quick_xml::de::*;
 use quick_xml::se::*;
 
 use schema::*;
+use crate::schema::*;
 use diesel::*;
 use diesel::prelude::*;
 use models::*;
 use diesel_demo2::*;
 
 fn main() -> std::result::Result<(), Box<dyn Error>> {
+    use schema::users::dsl::*;
     use schema::posts::dsl::*;
 
     let connection = establish_connection();
-    let users = users::table.load::<User>(&connection)?;
+    //let userlist = users::table.load::<User>(&connection)?;
+    let userlist = users.load::<User>(&connection)?;
     // variable name posts is not allowed because it's a type
-	let userposts = Post::belonging_to(&users)
+	let userposts = Post::belonging_to(&userlist)
 		.load::<Post>(&connection)?
-		.grouped_by(&users);
-	let data = users.into_iter().zip(userposts).collect::<Vec<_>>();
+		.grouped_by(&userlist);
+	let data = userlist.into_iter().zip(userposts).collect::<Vec<_>>();
 	// This is the way to serialize simple structs with serde
 	//let str = to_string(&data).unwrap(); println!("Line: {}", str);
 	let str = serde_json::to_string(&data)?;
@@ -38,9 +42,12 @@ fn main() -> std::result::Result<(), Box<dyn Error>> {
 	// no semikolon when returning values
 	
 	// from: https://github.com/diesel-rs/diesel/issues/89
-	let data2 = users.left_outer_joins(posts::table).load(&connection)
-		.group_by(|(user, post)| user)
-		.map(|(user, posts)| posts.into_iter().filter_map(|p| p).collect())
+	//let data2 = users::table.left_outer_join(posts::table).load(&connection)
+	let data2 = users.left_outer_join(posts).load(&connection)?
+		//.group_by(|(user, post)| user)
+		.group_by(|(users, posts)| users)
+		//.map(|(user, posts)| posts.into_iter().filter_map(|p| p).collect())
+		.map(|(users, posts)| posts.into_iter().filter_map(|p| p).collect())
 		.collect();
 	let str2 = serde_json::to_string(&data2)?;
 	println!("Line: {}", str);
