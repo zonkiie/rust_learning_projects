4877cb75cf47a7dee7f3149b945de070d774c151
Rainer Datzer <rdatz@gmx.de>
2021-10-07 04:49:51 +0200

    working on relations

--- seaorm_demo/src/main.rs
+++ seaorm_demo/src/main.rs
@@ -15,6 +15,27 @@ pub use entities::post::Entity as Post;
 mod setup;
 
 
+#[derive(Debug, Clone)]
+struct AppState {
+    templates: tera::Tera,
+    conn: DatabaseConnection,
+}
+
+pub async fn do_query()
+{
+    // get env vars
+    dotenv::dotenv().ok();
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
+    let host = env::var("HOST").expect("HOST is not set in .env file");
+    let port = env::var("PORT").expect("PORT is not set in .env file");
+    let server_url = format!("{}:{}", host, port);
+
+    // create post table if not exists
+    let conn = sea_orm::Database::connect(&db_url).await.unwrap();
+    let _ = setup::create_post_table(&conn).await;
+    let _ = setup::create_user_table(&conn).await;
+}
+
 fn main() {
-    println!("Hello, world!");
+    do_query();
 }
