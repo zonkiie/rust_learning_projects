7ff19384c2a07de846a6e8ad603d09ddc70514cc
Rainer Datzer <rdatz@gmx.de>
2021-12-19 04:00:38 +0100

    added slotfinder

--- slotfinder/src/main.rs
+++ slotfinder/src/main.rs
@@ -1,6 +1,9 @@
+#[allow(unused_imports)]
 use std::ops::Range;
-use slotfinder;
+mod slotfinder;
 
 fn main() {
-    println!("Hello, world!");
+    let arr = [(100..999), (10000..99999), (1000000..9999999)];
+    println!("Arr: {:?}", arr.to_vec());
+    
 }
--- slotfinder/src/slotfinder.rs
+++ slotfinder/src/slotfinder.rs
@@ -1,5 +1,11 @@
+#[allow(unused_imports)]
 use std::ops::Range;
 
-fn find_slot<T>(ranges: [&Range<T>], cand: T)->T {
-	
+fn find_next_slot<T>(ranges: &V^ec<Range<T>>, cand: T) ->T {
+	let mut found:T = ranges[0];
+	found
 }
+
+/*fn find_previous_slot<T>(ranges: [&Range<T>], cand: T) ->T {
+	
+}*/
