7c2c28a7bacdd0a21dafbe3fca3a11d24a3fac4b
Rainer Datzer <rdatz@gmx.de>
2021-12-19 04:53:59 +0100

    improved slotfinder

--- slotfinder/src/slotfinder.rs
+++ slotfinder/src/slotfinder.rs
@@ -2,7 +2,7 @@
 use std::ops::Range;
 
 fn find_next_slot<T>(ranges: &Vec<Range<T>>, cand: &T) -> T {
-	let mut sorted_ranges = ranges.sort();
+	let mut sorted_ranges = ranges.sort_by(|a, b| a.end.cmp(b.start).unwrap());
 	let found:T = ranges[0].start;
 	found
 }
