06e9aae57b4d7016402cdbaaad492b35e1fb0b45
Rainer Datzer <rdatz@gmx.de>
2021-12-20 03:08:48 +0100

    improved slotfinder

--- slotfinder/src/slotfinder.rs
+++ slotfinder/src/slotfinder.rs
@@ -1,11 +1,18 @@
 #[allow(unused_imports)]
 use std::ops::Range;
+#[allow(unused_imports)]
+use std::cmp::Ordering;
 
-fn find_next_slot<T>(ranges: &Vec<Range<T>>, cand: &T) -> T {
-	let mut sorted_ranges = ranges.sort_by(|a, b| a.end.cmp(b.start).unwrap());
-	let found:T = ranges[0].start;
+fn find_next_slot<i64>(ranges: &Vec<Range<i64>>, cand: &i64) -> i64 {
+	//let mut sorted_ranges = ranges.sort_by(|a, b| a.end.cmp(b.start).unwrap());
+	let mut sorted_ranges = ranges.sort_by(|a, b| a.cmp(b).unwrap());
+	let found:i64 = ranges[0].start;
 	found
 }
+
+/*fn find_next_slot<T>(ranges: [&Range<T>], cand: T) ->T {
+	let mut sorted_ranges = ranges.sort_by(|a, b| a.cmp(b).unwrap());
+}*/
 
 /*fn find_previous_slot<T>(ranges: [&Range<T>], cand: T) ->T {
 	
