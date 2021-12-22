8b7a75e7dd58b1da75395619ebe13cc03ee1c8c5
Rainer Datzer <rdatz@gmx.de>
2021-12-22 03:33:49 +0100

    working on order

--- slotfinder/src/slotfinder.rs
+++ slotfinder/src/slotfinder.rs
@@ -3,11 +3,11 @@ use std::ops::Range;
 #[allow(unused_imports)]
 use std::cmp::Ordering;
 
-pub fn find_next_slot(ranges: &mut Vec<Range<i64>>, cand: &i64) -> i64 {
+pub fn find_next_slot<T: Ord>(ranges: &mut Vec<Range<T>>, cand: &T) -> T {
 	//let mut sorted_ranges = ranges.sort_by(|a, b| a.end.cmp(b.start).unwrap());
 	let sorted_ranges = ranges.sort();//ranges.sort_by(|a, b| a.cmp(b));
 	println!("Sorted Arr: {:?}", sorted_ranges);
-	let found:i64 = ranges[0].start;
+	let found:T = ranges[0].start;
 	found
 }
 
