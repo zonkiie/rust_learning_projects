d4f5ddb0817a79941573255d059f4500c14df6a9
Rainer Datzer <rdatz@gmx.de>
2021-12-27 03:02:34 +0100

    working on slotfinder

--- slotfinder/src/main.rs
+++ slotfinder/src/main.rs
@@ -6,6 +6,6 @@ use slotfinder::*;
 fn main() {
     let mut arr:Vec<Range<i64>> = vec![(100..999), (10000..99999), (1000000..9999999), (1..10)];
     println!("Arr: {:?}", arr.to_vec());
-    let arr2 = find_next_slot(&mut arr, &100);
+    let arr2 = find_next_slot(&mut arr, 100);
     
 }
--- slotfinder/src/slotfinder.rs
+++ slotfinder/src/slotfinder.rs
@@ -3,13 +3,23 @@ use std::ops::Range;
 #[allow(unused_imports)]
 use std::cmp::Ordering;
 
-pub fn find_next_slot<T: Ord>(ranges: &mut Vec<Range<T>>, cand: &T) -> T {
+pub fn find_next_slot(ranges: &mut Vec<Range<i64>>, cand: i64) -> i64 {
 	//let mut sorted_ranges = ranges.sort_by(|a, b| a.end.cmp(b.start).unwrap());
-	let sorted_ranges = ranges.sort();//ranges.sort_by(|a, b| a.cmp(b));
+	let sorted_ranges = ranges.sort_by(|a, b| a.cmp(b));
+	//let sorted_ranges = ranges.sort_by(|a, b| a.start.cmp(&b.end));
+	println!("Sorted Arr: {:?}", sorted_ranges);
+	let found:i64 = ranges[0].start;
+	found
+}
+
+/*pub fn find_next_slot<T: Ord>(ranges: &mut Vec<Range<T>>, cand: &T) -> T {
+	//let mut sorted_ranges = ranges.sort_by(|a, b| a.end.cmp(b.start).unwrap());
+	//let sorted_ranges = ranges.sort();//ranges.sort_by(|a, b| a.cmp(b));
+	let sorted_ranges = ranges.sort_by(|a, b| a.start.cmp_by(b.end));
 	println!("Sorted Arr: {:?}", sorted_ranges);
 	let found:T = ranges[0].start;
 	found
-}
+}*/
 
 /*fn find_next_slot<T>(ranges: [&Range<T>], cand: T) ->T {
 	let mut sorted_ranges = ranges.sort_by(|a, b| a.cmp(b).unwrap());
